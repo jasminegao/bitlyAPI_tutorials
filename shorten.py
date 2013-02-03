e97981d0b688dd4434a203b022fb2ac775720590
Jasmine Gao <jasminegao93@gmail.com>
2013-02-03 16:03:11 -0500

    updated ACCESS_TOKEN

--- shorten.py
+++ shorten.py
@@ -1,10 +1,8 @@
 import requests
 import json
 
-ACCESS_TOKEN = "500752aa74ab91846753fc86c53f8e7cb27f5c81"
-
 query_params = {
-    'access_token': ACCESS_TOKEN,
+    'access_token': settings.ACCESS_TOKEN,
     'longUrl': "http://en.wikipedia.org/wiki/Tetraodontidae"}
     
 endpoint = "https://api-ssl.bitly.com/v3/shorten"
