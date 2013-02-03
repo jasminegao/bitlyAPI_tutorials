c50259f78f36f4ccf22b6391e3920838ea714eba
Jasmine Gao <jasminegao93@gmail.com>
2013-02-03 15:54:57 -0500

    updated ACCESS_TOKEN

--- bursting_phrases.py
+++ bursting_phrases.py
@@ -2,10 +2,8 @@ import requests
 import json
 import pprint
 
-ACCESS_TOKEN = "500752aa74ab91846753fc86c53f8e7cb27f5c81"
-
 def getBurstingPhrases():
-    query_params = {'access_token': ACCESS_TOKEN}
+    query_params = {'access_token': settings.ACCESS_TOKEN}
 
     endpoint = "https://api-ssl.bitly.com/v3/realtime/bursting_phrases"
     response = requests.get(endpoint, params = query_params)
